84bb23acda2efde84af71005f39610a7d8a75421
jherediagu <jherediagu@gmail.com>
2014-02-25 04:18:18 -0800

    creació sessio.java

---
+++ SupermercatsItteria/src/com/market/Form/sessio.java
@@ -0,0 +1,48 @@
+package com.market.Form;
+
+public class sessio {
+
+
+private String nom;
+private float preu;
+private int quantitat;
+
+// -------------------------------------------------------
+//  Creació de un nou producte
+// -------------------------------------------------------
+
+
+public Producte (String ProducteNom, double ProductePreu, int ProducteQuantitat)
+{
+  nom = ProducteNom;
+  preu = ProductePreu;
+  quantitat = ProducteQuantitat;
+
+}
+
+// -------------------------------------------------
+//   Retorna el preu per unitat del producte
+// -------------------------------------------------
+public double getPreu()
+{
+  return preu;
+}
+
+// -------------------------------------------------
+//   Retorna el nom del producte
+// -------------------------------------------------
+public String getNom()
+{
+  return nom;
+}
+
+// -------------------------------------------------
+//   Retorna la quantitat del producte
+// -------------------------------------------------
+public int getQuantitat()
+{
+  return quantitat;
+}
+
+
+} 

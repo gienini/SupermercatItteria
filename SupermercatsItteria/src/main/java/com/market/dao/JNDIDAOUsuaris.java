5cf97b9403cb72030a0e43a995e667ad9369d3c8
gienini <Alumno22@SBS-ALUMNO22>
2014-02-26 15:12:43 +0100

    Afegit JNDIDAO de usuaris

---
+++ SupermercatsItteria/src/main/java/com/market/dao/JNDIDAOUsuaris.java
@@ -0,0 +1,54 @@
+package com.market.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.market.VO.Usuari;
+
+public class JNDIDAOUsuaris implements IDAOUsuaris {
+
+    @Override
+    public void addUsuari(Usuari u) {
+
+        Connection con = ConnectionStatic.getConnection();
+        try {
+            PreparedStatement ps = con
+                    .prepareStatement("INSERT INTO usuaris VALUES('"
+                            + u.getNick() + "','" + u.getPass() + "', '"
+                            + u.getNom() + "', " + u.getEdat() + ", '"
+                            + u.getEmail() + "', '" + u.getTelefon()
+                            + "', null);");
+            ps.executeUpdate();
+
+            con.close();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean isLogin(Usuari u) {
+        Connection con = ConnectionStatic.getConnection();
+        if (u.getNick() == null || u.getPass() == null) {
+            return false;
+        }
+        try {
+            PreparedStatement ps = con
+                    .prepareStatement("SELECT COUNT(*) FROM usuaris WHERE nick='"
+                            + u.getNick() + "' AND pass='" + u.getPass() + "';");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            if (rs.getInt(1)==1){
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
